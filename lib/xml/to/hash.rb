03d870dc9c1dd76a7cadb84fb6678a59c635b0dc
Maarten <m.f.a.trompper@uva.nl>
2015-06-15 11:04:38 +0200

    Refactored some code

--- lib/xml/to/hash.rb
+++ lib/xml/to/hash.rb
@@ -6,7 +6,15 @@ module Xml
     module Hash
       VERSION = '1.0.3'
 
-      def add_children!(node, hash, blacklist=[])
+      def self.get_from_map_or_raise(map, int)
+        if map[int]
+          map[int]
+        else
+          raise "Could not handle #{int}"
+        end
+      end
+
+      def self.add_children!(node, hash, blacklist=[])
         unless node.respond_to? :node_type and blacklist.include? node.node_type
           if node.children and node.children.length > 0
             children_nodes=[]
@@ -16,6 +24,22 @@ module Xml
             hash[:children]=children_nodes
           end
         end
+      end
+
+      def self.set_object_array!(hash, meth)
+        if respond_to? meth and send(meth) and send(meth).length > 0
+          hash[meth] = []
+          array = send(meth)
+          case meth
+            when :entities, :elements, :notations
+              array = array.values
+            else
+          end
+          array.each do |el|
+            hash[meth] << el.to_hash
+          end
+        end
+        hash
       end
     end
   end
@@ -42,8 +66,6 @@ module Nokogiri
       end
     end
     class Namespace
-      include Xml::To::Hash
-
       def to_hash
         hash = {
             href: href
@@ -55,7 +77,18 @@ module Nokogiri
       end
     end
     class ElementContent
-      include Xml::To::Hash
+      OCCUR_TO_SYMBOL_MAP = {
+          ONCE => :once,
+          OPT => :opt,
+          MULT => :mult,
+          PLUS => :plus
+      }
+      TYPE_TO_SYMBOL_MAP={
+          PCDATA => :pcdata,
+          ELEMENT => :element,
+          SEQ => :seq,
+          OR => :or
+      }
 
       def to_hash
         hash = {}
@@ -66,109 +99,66 @@ module Nokogiri
           hash[:prefix] = prefix
         end
         if occur
-          case occur
-            when ONCE
-              hash[:occur] = :once
-            when OPT
-              hash[:occur] = :opt
-            when MULT
-              hash[:occur] = :mult
-            when PLUS
-              hash[:occur] = :plus
-            else
-              raise "Could not handle occur value #{occur}"
-          end
+          Xml::To::Hash.get_from_map_or_raise(OCCUR_TO_SYMBOL_MAP, occur)
         end
         if type
-          case type
-            when PCDATA
-              hash[:type] = :pcdata
-            when ELEMENT
-              hash[:type] = :element
-            when SEQ
-              hash[:type] = :seq
-            when OR
-              hash[:type] = :or
-            else
-              raise "Could not handle type value #{occur}"
-          end
+          Xml::To::Hash.get_from_map_or_raise(TYPE_TO_SYMBOL_MAP, type)
         end
-        add_children! self, hash
+        Xml::To::Hash::add_children! self, hash
         hash
       end
     end
     class Node
-      include Xml::To::Hash
-      # Returns node type as symbol instead of integer
-      def self.get_type(int)
-        case int
-          # Element node type, see Nokogiri::XML::Node#element?
-          when Nokogiri::XML::Node::ELEMENT_NODE
-            return :element
+      ELEMENT_TO_SYMBOL_MAP = {
+          # Element node type
+          Nokogiri::XML::Node::ELEMENT_NODE => :element,
           # Attribute node type
-          when Nokogiri::XML::Node::ATTRIBUTE_NODE
-            return :attribute
+          Nokogiri::XML::Node::ATTRIBUTE_NODE => :attribute,
           # Text node type, see Nokogiri::XML::Node#text?
-          when Nokogiri::XML::Node::TEXT_NODE
-            return :text
+          Nokogiri::XML::Node::TEXT_NODE => :text,
           # CDATA node type, see Nokogiri::XML::Node#cdata?
-          when Nokogiri::XML::Node::CDATA_SECTION_NODE
-            return :cdata
+          Nokogiri::XML::Node::CDATA_SECTION_NODE => :cdata,
           # Entity reference node type
-          when Nokogiri::XML::Node::ENTITY_REF_NODE
-            return :entity_ref
+          Nokogiri::XML::Node::ENTITY_REF_NODE => :entity_ref,
           # Entity node type
-          when Nokogiri::XML::Node::ENTITY_NODE
-            return :entity
+          Nokogiri::XML::Node::ENTITY_NODE => :entity,
           # PI node type
-          when Nokogiri::XML::Node::PI_NODE
-            return :pi
+          Nokogiri::XML::Node::PI_NODE => :pi,
           # Comment node type, see Nokogiri::XML::Node#comment?
-          when Nokogiri::XML::Node::COMMENT_NODE
-            return :comment
+          Nokogiri::XML::Node::COMMENT_NODE => :comment,
           # Document node type, see Nokogiri::XML::Node#xml?
-          when Nokogiri::XML::Node::DOCUMENT_NODE
-            return :document
+          Nokogiri::XML::Node::DOCUMENT_NODE => :document,
           # Document type node type
-          when Nokogiri::XML::Node::DOCUMENT_TYPE_NODE
-            return :document_type
+          Nokogiri::XML::Node::DOCUMENT_TYPE_NODE => :document_type,
           # Document fragment node type
-          when Nokogiri::XML::Node::DOCUMENT_FRAG_NODE
-            return :document_fraf
+          Nokogiri::XML::Node::DOCUMENT_FRAG_NODE => :document_fragment,
           # Notation node type
-          when Nokogiri::XML::Node::NOTATION_NODE
-            return :notation
+          Nokogiri::XML::Node::NOTATION_NODE => :notation,
           # HTML document node type, see Nokogiri::XML::Node#html?
-          when Nokogiri::XML::Node::HTML_DOCUMENT_NODE
-            return :html_doc
+          Nokogiri::XML::Node::HTML_DOCUMENT_NODE => :html_doc,
           # DTD node type
-          when Nokogiri::XML::Node::DTD_NODE
-            return :dtd
+          Nokogiri::XML::Node::DTD_NODE => :dtd,
           # Element declaration type
-          when Nokogiri::XML::Node::ELEMENT_DECL
-            return :element_declaration
+          Nokogiri::XML::Node::ELEMENT_DECL => :element_declaration,
           # Attribute declaration type
-          when Nokogiri::XML::Node::ATTRIBUTE_DECL
-            return :attribute_declaration
+          Nokogiri::XML::Node::ATTRIBUTE_DECL => :attribute_declaration,
           # Entity declaration type
-          when Nokogiri::XML::Node::ENTITY_DECL
-            return :entity_declaration
+          Nokogiri::XML::Node::ENTITY_DECL => :entity_declaration,
           # Namespace declaration type
-          when Nokogiri::XML::Node::NAMESPACE_DECL
-            return :namespace_declaration
+          Nokogiri::XML::Node::NAMESPACE_DECL => :namespace_declaration,
           # XInclude start type
-          when Nokogiri::XML::Node::XINCLUDE_START
-            return :xinclude_start
+          Nokogiri::XML::Node::XINCLUDE_START => :xinclude_start,
           # XInclude end type
-          when Nokogiri::XML::Node::XINCLUDE_END
-            return :xinclude_end
+          Nokogiri::XML::Node::XINCLUDE_END => :xinclude_end,
           # DOCB document node type
-          when Nokogiri::XML::Node::DOCB_DOCUMENT_NODE
-            return :docb_document
-          else
-            raise "Could not handle node type #{int}"
-        end
+          Nokogiri::XML::Node::DOCB_DOCUMENT_NODE => :docb_document
+      }
+
+      # Returns node type as symbol instead of integer
+      def self.get_symbol_for_type(int)
+        Xml::To::Hash.get_from_map_or_raise(ELEMENT_TO_SYMBOL_MAP, int)
       end
+
 
       def set_attributes(hash)
         hash = hash.clone
@@ -186,98 +176,77 @@ module Nokogiri
         hash
       end
 
+      def set_content_in_hash!(hash, blacklist=[])
+        unless blacklist.include? node_type
+          if respond_to? :content and content
+            c = content
+            if c.class == String
+              hash[:content] = c
+            elsif c.is_a? Nokogiri::XML::ElementContent
+              hash[:content] = c.to_hash
+            else
+              raise "Could not handle content class #{c.class}"
+            end
+          end
+        end
+        hash
+      end
+
+      def set_hash_if_responds!(hash, meth, blacklist=[])
+        unless blacklist.include? node_type
+          if respond_to? meth
+            val = send(meth)
+            if val
+              # if Node.get_type(node_type).to_s == val
+              #   puts "Consider blacklisting #{val} for #{meth}"
+              # end
+              if val.respond_to? :to_hash
+                val = val.to_hash
+              end
+              hash[meth] = val
+            end
+          end
+        end
+      end
+
       # Serialize this Node to a hash
       #
       # Example:
       #    >> Nokogiri::XML('<xml>hello</xml>').root.to_hash
       #    => {:type=>:element, :name=>"xml", :children=>[{:type=>:text, :content=>"hello"}]}
       def to_hash
-        def set_if_respond_to!(hash, meth, blacklist=[])
-          unless blacklist.include? node_type
-            if respond_to? meth
-              val = send(meth)
-              if val
-                # if Node.get_type(node_type).to_s == val
-                #   puts "Consider blacklisting #{val} for #{meth}"
-                # end
-                if val.respond_to? :to_hash
-                  val = val.to_hash
-                end
-                hash[meth] = val
-              end
-            end
-          end
-        end
-
-        # Helper functions
-        def set_object_array!(hash, meth)
-          if respond_to? meth and send(meth) and send(meth).length > 0
-            hash[meth] = []
-            array = send(meth)
-            case meth
-              when :entities, :elements, :notations
-                array = array.values
-              else
-            end
-            array.each do |el|
-              hash[meth] << el.to_hash
-            end
-          end
-          hash
-        end
-
-        def set_content!(hash, blacklist=[])
-          unless blacklist.include? node_type
-            if respond_to? :content and content
-              c = content
-              if c.class == String
-                hash[:content] = c
-              elsif c.is_a? Nokogiri::XML::ElementContent
-                hash[:content] = c.to_hash
-              else
-                raise "Could not handle content class #{c.class}"
-              end
-            end
-          end
-          hash
-        end
-
         # Initialize hash
         hash = {
-            type: Node.get_type(node_type),
+            type: Node.get_symbol_for_type(node_type),
         }
 
-        # Treat elements a little bit differently
-        case node_type
-          when Nokogiri::XML::Node::DTD_NODE
-            set_object_array!(hash, :elements)
-            set_object_array!(hash, :entities)
-            set_object_array!(hash, :notations)
-          when Nokogiri::XML::Node::ELEMENT_DECL
-          when Nokogiri::XML::Node::ENTITY_DECL
-            set_if_respond_to! hash, :original_content
-          else
+        # Treat DTD nodes a little bit differently
+        if node_type == Nokogiri::XML::Node::DTD_NODE
+          Xml::To::Hash.set_object_array!(hash, :elements)
+          Xml::To::Hash.set_object_array!(hash, :entities)
+          Xml::To::Hash.set_object_array!(hash, :notations)
         end
 
-        set_if_respond_to! hash, :name, [Node::DOCUMENT_NODE, Node::TEXT_NODE, Node::COMMENT_NODE]
-        set_if_respond_to! hash, :external_id # For DTD, entity declarations
-        set_if_respond_to! hash, :entity_type # For Entity declarations
-        set_if_respond_to! hash, :system_id # For Entity declarations
-        set_if_respond_to! hash, :attribute_type # For attribute declarations
-        set_if_respond_to! hash, :default # For attribute declarations
-        set_if_respond_to! hash, :enumeration # For attribute declarations
-        set_if_respond_to! hash, :element_type # For element declarations
-        set_if_respond_to! hash, :prefix # For element declarations
+        set_hash_if_responds! hash, :name, [Node::DOCUMENT_NODE, Node::TEXT_NODE, Node::COMMENT_NODE]
+        set_hash_if_responds! hash, :external_id # For DTD, entity declarations
+        set_hash_if_responds! hash, :original_content # For element and entity declarations
+        set_hash_if_responds! hash, :entity_type # For Entity declarations
+        set_hash_if_responds! hash, :system_id # For Entity declarations
+        set_hash_if_responds! hash, :attribute_type # For attribute declarations
+        set_hash_if_responds! hash, :default # For attribute declarations
+        set_hash_if_responds! hash, :enumeration # For attribute declarations
+        set_hash_if_responds! hash, :element_type # For element declarations
+        set_hash_if_responds! hash, :prefix # For element declarations
 
-        set_content! hash, [Node::DOCUMENT_NODE, Node::ELEMENT_NODE]
+        set_content_in_hash! hash, [Node::DOCUMENT_NODE, Node::ELEMENT_NODE]
         set_attributes! hash
-        set_if_respond_to! hash, :line
+        set_hash_if_responds! hash, :line
         if respond_to? :namespace and namespace
           hash[:namespace] = namespace.to_hash
         end
 
         # Recurse into children
-        add_children! self, hash, [Nokogiri::XML::Node::DTD_NODE, Node::ATTRIBUTE_NODE]
+        Xml::To::Hash.add_children! self, hash, [Nokogiri::XML::Node::DTD_NODE, Node::ATTRIBUTE_NODE]
         hash
       end
     end
